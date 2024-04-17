c63887f9cf6c873b4d94d3c4eba14e62409ba30b
dwh77 <dwh1998@vt.edu>
2024-04-17 16:31:08 -0400

    getting weibul transformation together for wind data

--- Scripts/01_Data_Comp/2_NLDAS_Met_data_fill.R
+++ Scripts/01_Data_Comp/2_NLDAS_Met_data_fill.R
@@ -9,6 +9,7 @@ library(ggpmisc) #equations on ggplot
 library(Metrics) #rmse
 
 #### Read in and format data ####
+
 #Read in data 
 nldas <- read_csv("./Data/Carey2022_glm/FCR_GLM_NLDAS_010113_123119_GMTadjusted.csv")
 
@@ -34,6 +35,220 @@ fcrmet_filt <- fcrmet %>%
 
 joinedSW <- left_join(nldas_filt, fcrmet_filt, by = c("time" = "Datetime"))
 
+#### Weibul correction of windspeed #### 
+
+##pdf plots 
+head(joinedSW)
+
+joinedSW |> 
+  select(time, WindSpeed, Wind_hourly) |> 
+  rename(WindSpeed_NLDAS = WindSpeed) |> 
+  pivot_longer(-1) |> 
+  ggplot()+
+  geom_density(aes(value, fill = name))
+
+
+##calculate weibul for local wind speed
+data <- data.frame(x = seq(0, 15, length.out = 100))
+
+#remove NAs
+localSW_noNA <- joinedSW |> filter(!is.na(Wind_hourly))
+#estimate weibull paramaters 
+local_params <- EnvStats::eweibull(localSW_noNA$Wind_hourly, method = "mle")
+local_shape <- local_params$parameters[1]
+local_scale <- local_params$parameters[2]
+
+data$WD_local_fullYear <-
+  dweibull(data$x, shape = local_shape, scale = local_scale)
+
+##calculate weibul for NLDAS wind speed
+#remove NAs
+NLDASSW_noNA <- joinedSW |> filter(!is.na(WindSpeed))
+#estimate weibull paramaters 
+nldas_params <- EnvStats::eweibull(NLDASSW_noNA$WindSpeed, method = "mle")
+nldas_shape <- nldas_params$parameters[1]
+nldas_scale <- nldas_params$parameters[2]
+
+data$WD_nldas_fullYear <-
+  dweibull(data$x, shape = nldas_shape, scale = nldas_scale)
+
+##weibul for leaf on 
+joinedSW_leafon <- joinedSW |> 
+  mutate(month = month(time)) |> 
+  filter(month %in% c(5,6,7,8,9,10)) |> 
+  filter(!is.na(Wind_hourly), !is.na(WindSpeed))
+
+nldas_params_leafon <- EnvStats::eweibull(joinedSW_leafon$WindSpeed, method = "mle")
+nldas_shape_leafon <- nldas_params_leafon$parameters[1]
+nldas_scale_leafon <- nldas_params_leafon$parameters[2]
+
+data$WD_nldas_leafon <-
+  dweibull(data$x, shape = nldas_shape_leafon, scale = nldas_scale_leafon)
+
+
+local_params_leafon <- EnvStats::eweibull(joinedSW_leafon$Wind_hourly, method = "mle")
+local_shape_leafon <- local_params_leafon$parameters[1]
+local_scale_leafon <- local_params_leafon$parameters[2]
+
+data$WD_local_leafon <-
+  dweibull(data$x, shape = local_shape_leafon, scale = local_scale_leafon)
+
+
+#weibul for leaf off
+joinedSW_leafoff <- joinedSW |> 
+  mutate(month = month(time)) |> 
+  filter(month %in% c(12,1,2,3))|> 
+  filter(!is.na(Wind_hourly), !is.na(WindSpeed))
+
+nldas_params_leafoff <- EnvStats::eweibull(joinedSW_leafoff$WindSpeed, method = "mle")
+nldas_shape_leafoff <- nldas_params_leafoff$parameters[1]
+nldas_scale_leafoff <- nldas_params_leafoff$parameters[2]
+
+data$WD_nldas_leafoff <-
+  dweibull(data$x, shape = nldas_shape_leafoff, scale = nldas_scale_leafoff)
+
+local_params_leafoff <- EnvStats::eweibull(joinedSW_leafoff$Wind_hourly, method = "mle")
+local_shape_leafoff <- local_params_leafoff$parameters[1]
+local_scale_leafoff <- local_params_leafoff$parameters[2]
+
+data$WD_local_leafoff <-
+  dweibull(data$x, shape = local_shape_leafoff, scale = local_scale_leafoff)
+
+data |> 
+  pivot_longer(-1) |> 
+  ggplot()+
+  geom_line(aes(x = x, y = value, color = name), size = 1.5)+
+  labs(x = "Wind Speed (m/s)", y = "Density", color = "Data and Grouping")+
+  theme_classic()
+
+# #nldas
+# EnvStats::eweibull(NLDASSW_noNA$WindSpeed, method = "mle") #all 
+# EnvStats::eweibull(joinedSW_leafon$WindSpeed, method = "mle") #leaf on
+# EnvStats::eweibull(joinedSW_leafoff$WindSpeed, method = "mle") #leaf on
+# 
+# #local
+# EnvStats::eweibull(localSW_noNA$Wind_hourly, method = "mle") #all
+# EnvStats::eweibull(joinedSW_leafon$Wind_hourly, method = "mle") #leaf on
+# EnvStats::eweibull(joinedSW_leafoff$Wind_hourly, method = "mle") #leaf off
+
+
+
+
+# joinedSW$weibull_localwind <- dweibull(joinedSW$Wind_hourly, shape = local_shape, scale = local_scale)
+# joinedSW$weibull_nldaswind <- dweibull(joinedSW$WindSpeed, shape = nldas_shape, scale = nldas_scale)
+# joinedSW$weibull_nldas_localparmas <- dweibull(joinedSW$WindSpeed, shape = local_shape, scale = local_scale)
+# 
+# 
+# joinedSW |> 
+#   ggplot()+
+#   geom_line(aes(x = Wind_hourly, y = weibull_localwind, color = "local"))+
+#   geom_line(aes(x = WindSpeed, y = weibull_nldaswind, color = "nldas"))+
+#   geom_line(aes(x = WindSpeed, y = weibull_nldas_localparmas, color = "nldas_localparam"))
+
+##get df of percentiles for local wind
+#function to get 
+# z<-EnvStats::eqweibull(localSW_noNA$Wind_hourly, p = 0.9)
+# z$quantiles[1]
+
+percentiles <- seq(0.001, 0.999, by = 0.001)
+x <- c(1:999)
+
+weibul_local_df <- data.frame("Percentile" = percentiles, "x_local" = x)
+
+for (i in (weibul_local_df_2$Percentile)) {
+  
+
+   z <- EnvStats::eqweibull(localSW_noNA$Wind_hourly, p = i)
+   y <- z$quantiles[1]
+   
+   weibul_local_df <- weibul_local_df |> 
+     mutate(x_local = ifelse(Percentile == i, y, x_local))
+ 
+}
+
+##get df of percentiles for local wind
+##old function
+# percentiles <- seq(1, 99, by = 1)
+# weibul_nldas_df <- setNames(data.frame(matrix(ncol = 2, nrow = 99)), c("Percentile", "x_nldas"))
+
+# for (i in 1:length(percentiles)) {
+#   z <- EnvStats::eqweibull(NLDASSW_noNA$WindSpeed, p = i/100)
+#   y <- z$quantiles[1]
+#   
+#   weibul_nldas_df$Percentile[i] <- i
+#   weibul_nldas_df$x_nldas[i] <- y
+#   
+# }
+
+weibul_nldas_df <- data.frame("Percentile" = percentiles, "x_nldas" = x)
+
+for (i in (weibul_nldas_df_2$Percentile)) {
+  
+  
+  z <- EnvStats::eqweibull(NLDASSW_noNA$WindSpeed, p = i)
+  y <- z$quantiles[1]
+  
+  weibul_nldas_df <- weibul_nldas_df |> 
+    mutate(x_nldas = ifelse(Percentile == i, y, x_nldas))
+  
+}
+
+
+
+
+
+
+weibul_percentil_bind <- left_join(weibul_local_df, weibul_nldas_df, by = "Percentile")
+
+
+
+### bind wind data to webiul percentiles
+
+weibul_percentil_bind_local <- weibul_percentil_bind |>
+  select(Percentile, x_local) |>
+  mutate(x_local = round(x_local, digits = 3))
+
+weibul_percentil_bind_nldas <- weibul_percentil_bind |>
+  select(Percentile, x_nldas) |>
+  mutate(x_nldas = round(x_nldas, digits = 3))
+
+# 
+# head(joinedSW)
+# 
+# local_wind <- joinedSW |> 
+#   select(time, Wind_hourly) |> 
+#   mutate(Wind_hourly = round(Wind_hourly, digits = 2))
+# 
+# local_wind_percent <- left_join(local_wind, weibul_percentil_bind_local, by = c("Wind_hourly" = "x_local")) |> 
+#   filter(!is.na(Wind_hourly))
+
+nldas_wind <- joinedSW |>
+  select(time, WindSpeed) |>
+  mutate(WindSpeed = round(WindSpeed, digits = 3))
+
+
+##put nldas wind speed to percentile 
+ldt <- data.table::data.table( nldas_wind , key = "WindSpeed" )
+dt <- data.table::data.table( weibul_percentil_bind_nldas , key = "x_nldas" )
+
+zz <- dt[ ldt , list(time, WindSpeed, Percentile , x_nldas  ) , roll = "nearest" ]
+
+
+## now we have each nldas time lined up to a percentile, we'll join the local wind percentile data 
+corrected_nldasWIND <- left_join(zz, weibul_percentil_bind_local, by = "Percentile")
+
+corrected_nldasWIND |> 
+  ggplot()+
+  geom_density(aes(x_local))+ xlim(0,15)
+
+
+###trials 
+quantile(localSW_noNA$Wind_hourly, probs = seq(0.01, 0.99, by = 0.01))
+
+
+quantile(7, EnvStats::eweibull(NLDASSW_noNA$WindSpeed, method = "mle"))
+
+quantile(joinedSW$WindSpeed, rnorm(joinedSW$Wind_hourly))
 
 #### Comparing NLDAS to FCR shortwave data ####
 
